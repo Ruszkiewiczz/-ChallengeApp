4399bcc74a017bb7bc247a8b57a6b61c61f6a29d
Ruszkiewiczz <misioolsztyn@wp.pl>
2023-01-18 13:04:50 +0100

    Zadanie domowe dzien 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//zadanie domowe dzien 4
+
+
+string name = "Ewa";
+char sex = 'K';
+var age = 35;
+
+if (sex == 'K' && age > 40)
+{
+
+    Console.WriteLine("Kobieta ponizej 40 lat");
+}
+else if (name == "Ewa" && age == 35)
+{
+
+    Console.WriteLine("Jestem Ewa lat 35");
+
+}
+else if (age < 18 && sex=='M')
+{
+
+    Console.WriteLine("Niepelnoletni mezczyzna");
+}
+    
+else
+{
+    Console.WriteLine("Pelnoletni mezczyzna");
+}
