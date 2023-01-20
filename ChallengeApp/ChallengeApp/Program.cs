88c2ca82723d03baf70b177f038fd935d062f3eb
Ruszkiewiczz <misioolsztyn@wp.pl>
2023-01-20 14:53:03 +0100

    Zadanie domowe dzien 5

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,28 +1,78 @@
-﻿//zadanie domowe dzien 4
+﻿//zadanie domowe dzien 5
+int[] grades = new int[20];
 
+List<string> names = new List<string>();
+names.Add("Adam");
+names.Add("Adam");
+names.Add("Adam");
+names.Add("Michal");
+names.Add("Adam");
+names.Add("Monika");
+names.Add("Olaf");
+names.Add("Adam");
+names.Add("Olaf");
+names.Add("Jola");
+names.Add("Adam");
+names.Add("Monika");
+names.Add("Adam");
+names.Add("Olaf");
+names.Add("Michal");
+names.Add("Monika");
+names.Add("Michal");
+names.Add("Olaf");
+names.Add("Jola");
+names.Add("Michal");
 
-string name = "Ewa";
-char sex = 'K';
-var age = 35;
+var Adam  = 0;
+var Michal = 0;
+var Jola = 0;
+var Olaf = 0;
+var Monika = 0;
 
-if (sex == 'K' && age > 40)
+for (var i=0; i<names.Count; i++)
 {
-
-    Console.WriteLine("Kobieta ponizej 40 lat");
+    Console.WriteLine(names[i]);
 }
-else if (name == "Ewa" && age == 35)
-{
 
-    Console.WriteLine("Jestem Ewa lat 35");
+    Console.WriteLine("==========");
 
-}
-else if (age < 18 && sex=='M')
-{
 
-    Console.WriteLine("Niepelnoletni mezczyzna");
-}
-    
-else
-{
-    Console.WriteLine("Pelnoletni mezczyzna");
-}
+for (var i = 0; i < names.Count; i++)
+
+    if (names[i] == "Adam")
+        Adam++;
+    {
+        Console.WriteLine($"Adam:   " + Adam);
+    }
+
+for (var i = 0; i < names.Count; i++)
+
+    if (names[i] == "Michal")
+        Michal++;
+    {
+        Console.WriteLine($"Michal: " + Michal);
+    }
+
+for (var i = 0; i < names.Count; i++)
+
+    if (names[i] == "Jola")
+        Jola++;
+    {
+        Console.WriteLine($"Jola:   " + Jola);
+    }
+
+for (var i = 0; i < names.Count; i++)
+
+    if (names[i] == "Olaf")
+        Olaf++;
+    {
+        Console.WriteLine($"Olaf:   " + Olaf);
+    }
+
+for (var i = 0; i < names.Count; i++)
+
+    if (names[i] == "Monika")
+        Monika++;
+    {
+        Console.WriteLine($"Monika: " + Monika);
+    }
