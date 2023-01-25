7c36b194b5a982a6badd6efa50cb8cecf9624b6e
Ruszkiewiczz <misioolsztyn@wp.pl>
2023-01-25 14:33:46 +0100

    zadanie domowe dzien 5

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,71 +1,73 @@
-﻿//zadanie domowe dzien 5
-int[] grades = new int[20];
+﻿// zadanie domowe dzien 5
+using System.Diagnostics.Metrics;
 
-List<string> names = new List<string>();
-names.Add("Adam");
-names.Add("Adam");
-names.Add("Adam");
-names.Add("Michal");
-names.Add("Adam");
-names.Add("Monika");
-names.Add("Olaf");
-names.Add("Adam");
-names.Add("Olaf");
-names.Add("Jola");
-names.Add("Adam");
-names.Add("Monika");
-names.Add("Adam");
-names.Add("Olaf");
-names.Add("Michal");
-names.Add("Monika");
-names.Add("Michal");
-names.Add("Olaf");
-names.Add("Jola");
-names.Add("Michal");
+int numbers = 34668129;
+string numberAsString = numbers.ToString();
+char[] letters = numberAsString.ToCharArray();
 
-var AdamCount = 0;
-var MichalCount = 0;
-var JolaCount = 0;
-var OlafCount = 0;
-var MonikaCount = 0;
+int counter0 = 0;
+int counter1 = 0;
+int counter2 = 0;
+int counter3 = 0;
+int counter4 = 0;
+int counter5 = 0;
+int counter6 = 0;
+int counter7 = 0;
+int counter8 = 0;
+int counter9 = 0;
 
-for (var i=0; i<names.Count; i++)
+foreach (char letter in letters)
 {
-    Console.WriteLine(names[i]);
+    if (letter == '0')
+    {
+        counter0++;
+    }
+    else if (letter == '1')
+    {
+        counter1++;
+    }
+    else if (letter == '2')
+    {
+        counter2++;
+    }
+    else if (letter == '3')
+    {
+        counter3++;
+    }
+    else if (letter == '4')
+    {
+        counter4++;
+    }
+    else if (letter == '5')
+    {
+        counter5++;
+    }
+    else if (letter == '6')
+    {
+        counter6++;
+    } 
+    else if (letter == '7')
+    {
+        counter7++;
+    }
+    else if (letter == '8')
+    {
+        counter8++;
+    }
+    else if (letter == '9')
+    {
+        counter9++;
+    }
 }
 
-    Console.WriteLine("==========");
-
-
-for (var i = 0; i < names.Count; i++)
-{
-    if (names[i] == "Adam")
-    {
-        AdamCount++;
-    }
-
-    else if (names[i] == "Michal")
-    {
-        MichalCount++;
-    }
-
-    else if (names[i] == "Jola")
-    {
-        JolaCount++;
-    }
-
-    else if (names[i] == "Olaf")
-    {
-        OlafCount++;
-    }
-    else if (names[i] == "Monika")
-    { 
-        MonikaCount++;
-    }    
-}
-
-Console.WriteLine("Adam:   " + AdamCount);
-Console.WriteLine("Michal: " + MichalCount);
-Console.WriteLine("Jola:   " + JolaCount);
-Console.WriteLine("Olaf:   " + OlafCount);
-Console.WriteLine("Monika: " + MonikaCount);
+Console.WriteLine($"Number0 => {counter0}");
+Console.WriteLine($"Number1 => {counter1}");
+Console.WriteLine($"Number2 => {counter2}");
+Console.WriteLine($"Number3 => {counter3}");
+Console.WriteLine($"Number4 => {counter4}");
+Console.WriteLine($"Number5 => {counter5}");
+Console.WriteLine($"Number6 => {counter6}");
+Console.WriteLine($"Number7 => {counter7}");
+Console.WriteLine($"Number8 => {counter8}");
+Console.WriteLine($"Number9 => {counter9}");
+    
